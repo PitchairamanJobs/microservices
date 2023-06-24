83369548bed8bdfad3112b0d0ec10d802d47167e
PitchairamanJobs <123750727+PitchairamanJobs@users.noreply.github.com>
2023-06-24 17:12:47 +0530

    Update App.jsx

--- ui-web-app-reactjs/src/components/App.jsx
+++ ui-web-app-reactjs/src/components/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
 
   const handleApiCall = async (e) => {
     // const url = 'http://localhost:9999/'+e.target.name;
-    let url = 'http://3.91.90.32:32470/'+e.target.name
+    let url = 'http://65.2.168.211:32470/'+e.target.name
 
     displayData.url=url
     let options = {}
